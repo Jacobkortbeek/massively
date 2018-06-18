467b2879ca8434cf5562ee3751a31a33b0db7e6c
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-06-18 21:48:11 +0100

    header logo

--- front-page.php
+++ front-page.php
@@ -21,9 +21,9 @@
 				<!-- make this the main nav on the wordpress menu -->
 					<nav id="nav">
 						<ul class="links">
-							<li class="active"><?php the_field( 'tab_heading' ); ?></li>
-							<li><?php the_field( 'tab_two_heading' ); ?></li>
-							<li><?php the_field( 'tab_three_heading' ); ?></li>
+							<li class="active"><a href="index.html">This is Massively</a></li>
+							<li><a href="generic.html">Generic Page</a></li>
+							<li><a href="elements.html">Elements Reference</a></li>
 						</ul>
 						<ul class="icons">
 							<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
