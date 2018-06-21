c2a6ae6bd16827b2f88690b9dc48e5c2d041b7a4
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-06-21 21:16:25 +0100

    menus

--- front-page.php
+++ front-page.php
@@ -103,7 +103,9 @@
 						<section class="posts test">
 							<?php if( $query->have_posts() ) : while( $query->have_posts() ) : $query->the_post(); ?>
 							<article class="resr">
-								<header <?php post_class( 0 === $featured_posts->current_post ? 'first' : 'major' ); ?>>
+								<header <?php if( $wp_query->current_post == $wp_query->post_count-1 ): ?>
+									class="major"
+								<?php endif; ?>>
 									<span class="date"><?php the_date('Y-m-d'); ?></span>
 									<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 								</header>
