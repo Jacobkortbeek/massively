9846384d580417614cd67ad59351e8e857622fa7
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-06-21 20:33:29 +0100

    menus

--- front-page.php
+++ front-page.php
@@ -101,7 +101,7 @@
 						<!-- Posts -->
 						<section class="posts test">
 							<?php if( $query->have_posts() ) : while( $query->have_posts() ) : $query->the_post(); ?>
-							<article>
+							<article class="resr">
 								<header>
 									<span class="date"><?php the_date('Y-m-d'); ?></span>
 									<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
