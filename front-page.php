670efb349f3683ad750e235705f7724a13b40ae5
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-06-21 15:52:17 +0100

    menus

--- front-page.php
+++ front-page.php
@@ -38,7 +38,7 @@
 						</ul> -->
 						<ul class="icons">
 							<?php
-								$social = get_field('social_media');
+								$social = get_field('options', 'social_media');
 
 								if( $social ): ?>
 									<p>THIS IS HERE</p>
