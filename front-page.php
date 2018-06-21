ac0690aa2eeff1f6735e90a6006b15065e1f06b7
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-06-21 16:28:03 +0100

    menus

--- front-page.php
+++ front-page.php
@@ -42,9 +42,11 @@
 
 								if( $social ): ?>
 									<?php $twitter = get_field('twitter', 'options');
-										if( $twitter )?>
+										if( $twitter ): ?>
 											<p>This is twitter</p>
 										<?php endif; ?>
+								<?php else: ?>
+								<p>This is not here</p>
 									<?php endif; ?>
 							<!-- Make these if statements for the options social media panel -->
 							<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
