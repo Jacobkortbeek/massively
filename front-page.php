67d8f5025a99bc1974aaf611e91428d0dbd09db1
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-06-21 21:44:36 +0100

    menus

--- front-page.php
+++ front-page.php
@@ -106,7 +106,7 @@
 									<?php elseif: ?>
 								<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
-								<?php $i++; endif; endwhile; endif; wp_reset_postdata(); ?>
+								<?php endif; $i++; endwhile; endif; wp_reset_postdata(); ?>
 
 
 
