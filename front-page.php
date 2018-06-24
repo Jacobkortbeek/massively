b125870b71c3b57b12898a8e2b2a20950cd43274
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-06-24 20:27:50 +0100

    menus

--- front-page.php
+++ front-page.php
@@ -76,7 +76,7 @@ Here is the pagination
 						    'post_type' => 'post',
 						    'posts_per_page' => $num_posts,
 								'orderby' => 'post_date',
-								'paged' => 3
+								'paged' => 2
 
 						  );
 
