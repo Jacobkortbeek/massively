8df40ed5ed0e63b212d1fc14e48e15aa7820504e
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-06-24 15:55:33 +0100

    menus

--- front-page.php
+++ front-page.php
@@ -117,7 +117,7 @@
 											<nav class="pagination">
 												<?php pagination_bar(); ?>
 											</nav>
-								<?php endif;  endwhile; posts_nav_link(); wp_reset_query(); endif; wp_reset_postdata(); ?>
+								<?php endif;  endwhile; posts_nav_link(); wp_reset_query(); endif; ?>
 							</section>
 
 						<!-- Footer -->
