08e624b18a0a6c30d4956d34ce8062c903b5e714
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-06-24 15:02:25 +0100

    menus

--- functions.php
+++ functions.php
@@ -4,22 +4,42 @@
 
   function pagination_bar() {
 
-    global $wp_query;
+    global $wp_query, $wp_rewrite;
     $published_posts = wp_count_posts()->publish;
     $posts_per_page = get_option('posts_per_page');
-
-    $total_pages = ceil($published_posts / $posts_per_page);;
-
-    if ($total_pages > 1){
-        $current_page = max(1, get_query_var('paged'));
-
-        echo paginate_links(array(
-            'base' => get_pagenum_link(1) . '%_%',
-            'format' => '/page/%#%',
-            'current' => $current_page,
+     $total_pages = ceil($published_posts / $posts_per_page);
+        $wp_query->query_vars['paged'] > 1 ? $current = $wp_query->query_vars['paged'] : $current = 1;
+        $pagination = array(
+            'base' => @add_query_arg('page','%#%'),
+            'format' => '',
             'total' => $total_pages,
-        ));
-    }
+            'current' => $current,
+            'show_all' => false,
+            'mid_size' => 0,
+            'prev_text'    => __('<'),
+			'next_text'    => __('>'),
+            'type' => 'list'
+        );
+        if ( $wp_rewrite->using_permalinks() ) $pagination['base'] = user_trailingslashit( trailingslashit( remove_query_arg( 's', get_pagenum_link( 1 ) ) ) . 'page/%#%/', 'paged' );
+        if ( !empty($wp_query->query_vars['s']) ) $pagination['add_args'] = array( 's' => get_query_var( 's' ) );
+        echo paginate_links( $pagination );
+
+    // global $wp_query;
+    // $published_posts = wp_count_posts()->publish;
+    // $posts_per_page = get_option('posts_per_page');
+    //
+    // $total_pages = ceil($published_posts / $posts_per_page);;
+    //
+    // if ($total_pages > 1){
+    //     $current_page = max(1, get_query_var('paged'));
+    //
+    //     echo paginate_links(array(
+    //         'base' => get_pagenum_link(1) . '%_%',
+    //         'format' => '/page/%#%',
+    //         'current' => $current_page,
+    //         'total' => $total_pages,
+    //     ));
+    // }
 
 
 
