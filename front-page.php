8f669517d2f87c0396cd6863c379b7eb6b69535f
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-06-24 20:59:54 +0100

    menus

--- front-page.php
+++ front-page.php
@@ -128,6 +128,10 @@ Here is the pagination
 								<?php echo paginate_links(array(
 									'total' => $query->max_num_pages
 								)); ?>
+								var Paged<?php echo $query->$paged ?>
+								query Paged<?php echo $query->get_query_var( 'paged' ); ?>
+								query Page<?php echo $query->get_query_var( 'page' ); ?>
+
 
 							<?php endif; wp_reset_postdata(); ?>
 
