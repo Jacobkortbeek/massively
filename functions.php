23a8d0702862db302760866419cee4d7c1ada2af
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-06-24 15:24:15 +0100

    menus

--- functions.php
+++ functions.php
@@ -16,7 +16,7 @@
 
         echo paginate_links(array(
             'base' => get_pagenum_link(1) . '%_%',
-            'format' => '/page/%#%',
+            // 'format' => '/page/%#%',
             'current' => $current_page,
             'total' => $total_pages,
             'paged' => $paged,
