5206bcdafaeacea70be144dfcd6fb43c410951f9
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-06-24 12:45:32 +0100

    menus

--- front-page.php
+++ front-page.php
@@ -129,6 +129,18 @@
 								</div>
 
 								<nav class="pagination">
+									<?php
+global $wp_query;
+
+$big = 999999999; // need an unlikely integer
+
+echo paginate_links( array(
+	'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+	'format' => '?paged=%#%',
+	'current' => max( 1, get_query_var('paged') ),
+	'total' => $wp_query->max_num_pages
+) );
+?>
 									<?php global $wp_query;
 echo $wp_query->max_num_pages; ?>
 									TEST
