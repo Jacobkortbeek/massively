c3ae4276ece6b94c822fec519167b28573e7fefc
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-06-24 15:19:43 +0100

    menus

--- functions.php
+++ functions.php
@@ -21,6 +21,22 @@
         ));
     }
 
+    if ( get_query_var('paged') ) {
+
+$paged = get_query_var('paged');
+
+} elseif ( get_query_var('page') ) {
+
+$paged = get_query_var('page');
+
+} else {
+
+   $paged = 1;
+
+}
+
+query_posts( array( 'post_type' => 'post', 'paged' => $paged ) );
+
 
 
     // $total = ceil($published_posts / $posts_per_page);;
