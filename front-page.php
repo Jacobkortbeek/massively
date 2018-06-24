b441cd11c2a9f53329dc3e98c03dbbbfd2813a70
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-06-24 14:15:20 +0100

    menus

--- front-page.php
+++ front-page.php
@@ -133,9 +133,12 @@
 									$query=new WP_Query("showposts=1&suppress_filters=0");
 									echo $query->found_posts; ?>),
 									post per page (<?php echo $wp_query->posts_per_page; ?>),
-									 max num pages (<?php echo $wp_query->max_num_pages; ?>),
+									 max num pages (<?php global $wp_query echo $wp_query->max_num_pages; ?>),
 									TEST
 									<?php pagination_bar(); ?>
+
+
+
 								</nav>
 
 							</footer>
