41dedfe1e3c52260ed9ab785b0feec70562496c5
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-06-24 14:21:44 +0100

    menus

--- front-page.php
+++ front-page.php
@@ -136,7 +136,36 @@
 									 max num pages (<?php echo $wp_query->max_num_pages; ?>),
 									TEST
 									<?php pagination_bar(); ?>
+									<?php
+/* ------------------------------------------------------------------*/
+/* PAGINATION */
+/* ------------------------------------------------------------------*/
 
+//paste this where the pagination must appear
+
+global $wp_query;
+$total = $wp_query->max_num_pages;
+// only bother with the rest if we have more than 1 page!
+if ( $total > 1 )  {
+     // get the current page
+     if ( !$current_page = get_query_var('paged') )
+          $current_page = 1;
+     // structure of "format" depends on whether we're using pretty permalinks
+     if( get_option('permalink_structure') ) {
+	     $format = '&paged=%#%';
+     } else {
+	     $format = 'page/%#%/';
+     }
+     echo paginate_links(array(
+          'base'     => get_pagenum_link(1) . '%_%',
+          'format'   => $format,
+          'current'  => $current_page,
+          'total'    => $total,
+          'mid_size' => 4,
+          'type'     => 'list'
+     ));
+}
+?>
 
 
 								</nav>
