1fbb19b26e6b64d8bb1d1badd3e600da708dc52d
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-06-25 16:01:54 +0100

    menus

--- front-page.php
+++ front-page.php
@@ -180,6 +180,26 @@
 									<li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
 									<li><a href="#" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
 									<li><a href="#" class="icon alt fa-github"><span class="label">GitHub</span></a></li>
+									<!-- twitter -->
+									<?php $twitter = get_field('twitter', 'options');
+										if( $twitter ): ?>
+											<li><a href="<?php the_field('twitter', 'options'); ?>" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+									<?php endif; ?>
+									<!-- Facebook -->
+									<?php $facebook = get_field('facebook', 'options');
+										if( $facebook ): ?>
+											<li><a href="<?php the_field('facebook', 'options'); ?>" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+									<?php endif; ?>
+									<!-- Instagram -->
+									<?php $instagram = get_field('instagram', 'options');
+										if( $instagram ): ?>
+											<li><a href="<?php the_field('instagram', 'options'); ?>" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+									<?php endif; ?>
+									<!-- github -->
+									<?php $github = get_field('github', 'options');
+										if( $github ): ?>
+											<li><a href="<?php the_field('github', 'options'); ?>" class="icon fa-github"><span class="label">github</span></a></li>
+									<?php endif; ?>
 								</ul>
 							</section>
 						</section>
