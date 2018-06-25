e4c138ba943f57f5528c0fd03f8b134d08eb1b66
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-06-25 15:29:34 +0100

    menus

--- front-page.php
+++ front-page.php
@@ -132,18 +132,11 @@
 									'next_text' => 'Next'
 								)); ?>
 							</div>
-								Page(<?php  echo $paged;  ?>)
 
 							<?php endif; wp_reset_postdata(); ?>
 
-
-
-
-
-
-
-								<div class="pagination">
-									<!--<a href="#" class="previous">Prev</a>-->
+								<!-- <div class="pagination">
+									<a href="#" class="previous">Prev</a>
 									<a href="#" class="page active">1</a>
 									<a href="#" class="page">2</a>
 									<a href="#" class="page">3</a>
@@ -152,12 +145,9 @@
 									<a href="#" class="page">9</a>
 									<a href="#" class="page">10</a>
 									<a href="#" class="next">Next</a>
-								</div>
+								</div> -->
 
-								<nav class="pagination">
-									<?php pagination_bar(); ?>
-									<?php echo get_query_var( 'paged' ); ?>
-								</nav>
+
 
 							</footer>
 
