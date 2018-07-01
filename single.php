74f700f32c6953bc4ac20b40823c6ed0a86e0bc0
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-07-01 21:26:13 +0100

    single php header

--- single.php
+++ single.php
@@ -1,2 +1,25 @@
 <?php get_header(); ?>
-<h1>THIS IS THE SINGLE.PHP FILE</h1>
+
+<div id="main">
+
+
+
+
+
+
+  <!-- Post -->
+    <section class="post">
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <header class="major">
+          <span class="date"><?php the_date('Y-m-d'); ?></span>
+          <h1><?php the_title(); ?></h1>
+          <p><?php the_excerpt(); ?></p>
+        </header>
+        <div class="image main"><?php the_post_thumbnail( 'full' ); ?></div>
+        <p><?php the_content(); ?></p>
+      <?php endwhile; else : ?>
+        <p><?php _e( 'Sorry, no pages found.' ); ?>
+      <?php endif; ?>
+</div>
+
+<?php get_footer(); ?>
